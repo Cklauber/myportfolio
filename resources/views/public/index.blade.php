d24e6d5e3b54a92f57192bee8de47900fb12612c
clauber_rox <cklauber@gmail.com>
2019-01-30 23:15:16 -0700

    Can I spell?

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -11,7 +11,7 @@
                             <h1 class="title">Clauber Oliveira</h1>
                     </div>
                     <div class="subtitle">
-                        <h1 class="subtitle is-4">Welcome to my website. I am software/web developer with a passion for learning and providing my best to those who need my service.</h1>
+                        <h1 class="subtitle is-4">Welcome to my website. I am a software/web developer with a passion for learning and providing my best to those who need my service.</h1>
                         <h3 class="subtitle is-5">As you can see the website is still under development and you can follow along the progress in this <a href="https://trello.com/b/x5wARpYh/clauber-oliveira-portfolio" target="blank">trello board</a>.</h3>
                         <p>I will also be posting my progress in my <a href="https://github.com/Cklauber/myportfolio" target="blank">GitHub</a>, where I intend to make this project public for you to see and even use it.</p>
                         <p>While the website is not ready, please check ou my resumee and my linked in.</p>
